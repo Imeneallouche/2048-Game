ae1b6d57a3e0db958be35f40dd99c54b1f4000a2
Imène ALLOUCJE <https://github.com/Imeneallouche>
2023-02-11 01:30:51 +0100

    java project has been initialized, packages and src files and primary classes have been added

---
+++ 2048 Game/src/com/fatacubez/game/Game.java
@@ -0,0 +1,24 @@
+package com.fatacubez.game;
+
+import java.awt.event.keyEvent;
+import java.awt.event.KeyListener;
+import java.swing.JPanel;
+
+import javax.swing.JPanel;
+
+public class Game extends JPanel implements KeyListener, Runnable {
+    @Override
+    public void run() {
+
+    }
+
+    @Override
+    public void KeyPressed(keyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(keyEvent e) {
+
+    }
+}
